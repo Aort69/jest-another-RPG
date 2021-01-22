cc13f8406b2caeac4494a9a1ee48513fa8a00bde
Allec <allec.ortega69@Gmail.com>
2021-01-21 20:16:27 -0700

    tested potion use and damage from enemy and player

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -62,3 +62,34 @@ test('checks if player is alive or not', () => {
   
     expect(player.health).toBe(0);
   });
+// player attack value
+  test("gets player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+  
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+  });
+
+  //add potion to inventory 
+  test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+  
+    player.addPotion(new Potion());
+  
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+  });
+
+  //player uses potion (one is removed and stat is added)
+  test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+  
+    player.usePotion(1);
+  
+    expect(player.inventory.length).toBeLessThan(oldCount);
+  });
+
+  
